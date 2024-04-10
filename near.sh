31bfdd56af295e2ed118474e6b7244113b15e359
thilinanamal <29670007+thilinanamal@users.noreply.github.com>
2024-04-11 02:39:53 +0530

    Update near.sh

--- near.sh
+++ near.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://near.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://near.lava.build/lava-referer-f9477647-3aaa-4894-afe5-c403fadd0deb/"
 
 OUTPUT_DIR="logs"
 
